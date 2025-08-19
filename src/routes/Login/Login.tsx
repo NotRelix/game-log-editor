a4a1e495462ee9b8b9449455d64a6a89f37afdad
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-19 20:59:21 +0800

    feat: add login page

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -1,7 +1,58 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { useState, type FormEvent } from "react";
+import type { LoginType } from "../../types";
 
-export default Login
+const emptyLogin: LoginType = {
+  username: "",
+  password: "",
+};
+
+const Login = () => {
+  const [data, setData] = useState<LoginType>(emptyLogin);
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      const result = await response.json();
+      if (!result.success) {
+        console.error("Failed to fetch login");
+      } else {
+        localStorage.setItem("token", result.token);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          onChange={(e) => {
+            setData((prev) => ({ ...prev, username: e.target.value }));
+          }}
+          type="text"
+          id="username"
+          name="username"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          onChange={(e) => {
+            setData((prev) => ({ ...prev, password: e.target.value }));
+          }}
+          type="password"
+          id="password"
+          name="password"
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
